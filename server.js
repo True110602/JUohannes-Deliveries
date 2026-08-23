e96a07f579ca4e05e45015ee6593e3fa1602109e
unknown <nkosingcosi@gmail.com git config --global user.name true110602>
2026-08-23 12:54:00 +0200

    fetch URL to Render

--- server.js
+++ server.js
@@ -125,5 +125,5 @@ io.on('connection', (socket) => {
 // --- START SERVER ---
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
