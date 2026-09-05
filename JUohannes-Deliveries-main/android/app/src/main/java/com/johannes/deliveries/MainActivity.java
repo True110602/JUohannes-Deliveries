59a89e40f178d79573ba9452ff05a49642374e91
True110602 <65511521+True110602@users.noreply.github.com>
2026-09-05 14:21:47 +0200

    Delete JUohannes-Deliveries-main directory

--- JUohannes-Deliveries-main/android/app/src/main/java/com/johannes/deliveries/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.johannes.deliveries;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- JUohannes-Deliveries-main/public/js/config.js
+++
@@ -1,2 +0,0 @@
-// Single source of truth for where the backend API lives.
-window.API_BASE = 'https://juohannes-deliveries-2.onrender.com';
